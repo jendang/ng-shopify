d56eff85681e468d245ab7f2e998623172902516
jennydang <jenny.dang117@gmail.com>
2019-06-23 09:02:37 +0200

    dropdown categories and sorting elements

--- src/app/admin/product-form/product-form.component.ts
+++ src/app/admin/product-form/product-form.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { CategoryService } from 'src/app/category.service';
 
 @Component({
   selector: 'app-product-form',
   templateUrl: './product-form.component.html',
   styleUrls: ['./product-form.component.css']
 })
-export class ProductFormComponent implements OnInit {
+export class ProductFormComponent {
+  categories$;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(categoryService: CategoryService) {
+    this.categories$ = categoryService.getCategories()
   }
+
+  
 
 }
